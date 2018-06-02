2a92e0f6a722295b63acbdf4b5a75397c7b4aecf
cbviva <39769269+cbviva@users.noreply.github.com>
2018-06-02 13:01:23 +0800

    perms-whitelist

--- Vending/Android.mk
+++ Vending/Android.mk
@@ -1,4 +1,13 @@
 LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp-permissions-com.android.vending.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := Vending
 LOCAL_MODULE_TAGS := optional
